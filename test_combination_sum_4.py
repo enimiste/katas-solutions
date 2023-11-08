d8d87bd1e7a318bbf375cbbd59b2976ddb2acad0
Enimiste <enimiste@users.noreply.github.com>
2023-11-08 18:32:54 +0100

    Update test_combination_sum_4.py

--- test_combination_sum_4.py
+++ test_combination_sum_4.py
@@ -11,6 +11,12 @@ def combinations(nums: list[int], target: int) -> int:
     return 0
   if len(nums)==1:
     return 1 if target%nums[0]==0 else 0
+  if len(nums)==2:
+    case1 = combinations([nums[0]], target)
+    case2 = combinations(nums[1:], target-nums[0])
+    case3 = combinations(nums[1:], target)
+    return  case1 + case2 + case3
+          
   return 0
 
 # tests
@@ -25,6 +31,12 @@ def test_combinations_1_elem_equal_target():
 
 def test_combinations_1_elem_gt_target():
   assert combinations([2], 1)==0
+
+def test_combinations_1_elem_lt_target():
+  assert combinations([1], 2)==1
+
+def test_combinations_2_elem():
+  assert combinations([1, 2], 3)==2
 
 def test_combinations_1_elem_lt_target():
   assert combinations([1], 2)==1
