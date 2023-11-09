c3b238de6118d80ef24b1e824effca8bcbe545f3
Enimiste <enimiste@users.noreply.github.com>
2023-11-09 14:57:24 +0100

    Update test_combination_sum_4_count.py

--- test_combination_sum_4_count.py
+++ test_combination_sum_4_count.py
@@ -6,37 +6,48 @@ to target.
 Note that different sequences are counted as different 
 combinations.
 """
-def combinations_count(nums: list[int], target: int) -> int:
-  combs = combinations(nums, target)
-  print(combs)
-  return len(combs)
+def combinations(nums: list[int], target: int) -> int:
+  return len(combinations_helper(nums, target, []))
 
-def combinations(nums: list[int], target: int) -> list[list[int]]:
-  if target<=0:
+def combinations_helper(nums: list[int], target: int, so_far: list[list[int]]) -> list[list[int]]:
+  if target==0:
+    return so_far
+  if target<0:
     return []
-  if len(nums)==1:
-    return [[nums[0]]] if target%nums[0]==0 else []
-  case1 = combinations([nums[0]], target)
-  case2 = combinations(nums[1:], target-nums[0])
-  case3 = combinations(nums, target-nums[0])
-  case4 = combinations(nums[1:], target)
-  return  case1 + case2 + case3 + case4
-  # TODO : ignore order
+  if len(nums)>=1:
+    combs = []
+    for i in range(0, len(nums)):
+      # nums[i]
+      if len(so_far)==0:
+        new_so_far1 = [[nums[i]]]
+      else:
+        new_so_far1 = [sf + [nums[i]] for sf in so_far]
+      combs += combinations_helper(nums, target - nums[i], new_so_far1) 
+    return combs
+  return []
+
 # tests
 def test_dummy():
   assert True
 
 def test_combinations_1_elem_zero():
-  assert combinations_count([1], 0)==0
+  assert combinations([1], 0)==0
 
 def test_combinations_1_elem_equal_target():
-  assert combinations_count([1], 1)==1
+  assert combinations([1], 1)==1
 
 def test_combinations_1_elem_gt_target():
-  assert combinations_count([2], 1)==0
+  assert combinations([2], 1)==0
 
 def test_combinations_1_elem_lt_target():
-  assert combinations_count([1], 2)==1
+  assert combinations([1], 2)==1
+
+def test_combinations_2_elem_without_1():
+  """
+  (2,3)
+  (3,2)
+  """
+  assert combinations([2, 3], 5)==2
 
 def test_combinations_2_elem():
   """
@@ -44,13 +55,13 @@ def test_combinations_2_elem():
   (1,2)
   (2,1)
   """
-  assert combinations_count([1, 2], 3)==3
+  assert combinations([1, 2], 3)==3
 
 def test_combinations_2_elem_ko():
-  assert combinations_count([2, 4], 5)==0
+  assert combinations([2, 4], 5)==0
 
 def test_combinations_2_elem_ko():
-  assert combinations_count([2, 4], 5)==0
+  assert combinations([2, 4], 5)==0
 
 def test_combinations_3_elem_lt_target():
   """
@@ -62,7 +73,7 @@ def test_combinations_3_elem_lt_target():
   (2,1,1)
   (2,2)
   """
-  assert combinations_count([1,2,3], 4)==7
+  assert combinations([1,2,3], 4)==7
 
-def test_combinations_1_elem_lt_target():
-  assert combinations_count([1], 2)==1
+
+
